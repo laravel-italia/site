7322549a9e8ba386b089cd4b0a6b8da11ac2787f
Francesco Malatesta <hellofrancesco@gmail.com>
2015-11-30 22:20:44 +0100

    UserFactory can now create a User starting from social network data.

--- app/Entities/Factories/UserFactory.php
+++ app/Entities/Factories/UserFactory.php
@@ -30,6 +30,36 @@ class UserFactory
         $user->is_confirmed = false;
         $user->confirmation_code = sha1(microtime() . $user->email);
 
+        $user->provider = null;
+        $user->provider_id = null;
+
+        return $user;
+    }
+
+
+    /**
+     * Creates a new User instance, starting from name, email and social network provider data.
+     *
+     * @param $fullName
+     * @param $emailAddress
+     * @param $provider
+     * @param $providerId
+     * @return User
+     */
+    public static function createUserFromSocialNetwork($fullName, $emailAddress, $provider, $providerId)
+    {
+        $user = new User;
+
+        $user->name = $fullName;
+        $user->email = $emailAddress;
+        $user->password = '';
+
+        $user->is_confirmed = true;
+        $user->confirmation_code = '';
+
+        $user->provider = $provider;
+        $user->provider_id = $providerId;
+
         return $user;
     }
 }
--- tests/unit/Entities/Factories/UserFactoryTest.php
+++ tests/unit/Entities/Factories/UserFactoryTest.php
@@ -17,4 +17,22 @@ class UserFactoryTest extends TestCase
         $this->assertEquals(false, $user->is_confirmed);
         $this->assertEquals(40, strlen($user->confirmation_code));
     }
+
+    /**
+     * Test if the factory is able to create a User in the right way.
+     *
+     * @return void
+     */
+    public function testCanCreateSocialUser()
+    {
+        $user = \LaravelItalia\Entities\Factories\UserFactory::createUserFromSocialNetwork('Francesco', 'hey@hellofrancesco.com', 'facebook', 'FACEBOOK_ID');
+
+        $this->assertEquals('Francesco', $user->name);
+        $this->assertEquals('hey@hellofrancesco.com', $user->email);
+        $this->assertEquals(0, strlen($user->password));
+        $this->assertEquals(true, $user->is_confirmed);
+        $this->assertEquals(0, strlen($user->confirmation_code));
+        $this->assertEquals('facebook', $user->provider);
+        $this->assertEquals('FACEBOOK_ID', $user->provider_id);
+    }
 }
