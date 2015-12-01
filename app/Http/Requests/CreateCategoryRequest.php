bcfb86698885c0aecff7db17cf8ea9d9982e61a2
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-01 22:55:21 +0100

    Added the CreateCategoryRequest.

---
+++ app/Http/Requests/CreateCategoryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaravelItalia\Http\Requests;
+
+
+class CreateCategoryRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required'     => 'Non è stato specificato un nome per la categoria.'
+        ];
+    }
+}
