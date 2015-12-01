0d0580ded923036a50cd47ee2e65d82dd006caa9
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-01 15:39:19 +0100

    Added RoleChecker middleware.

---
+++ app/Http/Middleware/RoleChecker.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LaravelItalia\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class RoleChecker
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+
+    /**
+     * @param $request
+     * @param Closure $next
+     * @param ...$roles
+     * @return mixed
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        $user = $this->auth->user();
+
+        if(in_array($user->role->name, $roles))
+            return $next($request);
+
+        return redirect('/');
+    }
+}
