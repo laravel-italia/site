ceaea01ad491a6816d133b759ecc6fe321811fcc
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-02 16:40:58 +0100

    Added publication status methods to the Article model.

--- app/Entities/Article.php
+++ app/Entities/Article.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    public function isPublished()
+    {
+        return (!is_null($this->published_at));
+    }
+
+    public function getPublicationDate()
+    {
+        return $this->published_at;
+    }
+
+    public function publish($publicationDate)
+    {
+        $this->is_published = true;
+        $this->published_at = $publicationDate;
+    }
+
+    public function unpublish()
+    {
+        $this->is_published = false;
+        $this->published_at = null;
+    }
+
+    /* Relationship Methods */
+
     public function user()
     {
         return $this->belongsTo(User::class);
