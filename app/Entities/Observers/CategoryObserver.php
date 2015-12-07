108b70c900d5cf4fae0d302e111cd19d28a5bbfd
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-07 17:37:14 +0100

    Created the CategoryObserver to detach related articles from the category before delete it.

---
+++ app/Entities/Observers/CategoryObserver.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LaravelItalia\Entities\Observers;
+
+
+use LaravelItalia\Entities\Category;
+
+class CategoryObserver
+{
+
+    public function deleting(Category $category)
+    {
+        $category->articles()->detach();
+    }
+}
