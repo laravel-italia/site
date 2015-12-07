2a7da3fb16d05ce1a57aff394de1740de3a2222e
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-07 08:52:04 +0100

    Created the database migration file for media.

---
+++ database/migrations/2015_12_06_222838_create_media_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('url');
+            $table->integer('user_id')->unsigned();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('media');
+    }
+}
