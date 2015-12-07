a88652261ec078e54933967ce309b5746a6cca65
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-08 00:34:32 +0100

    Added the starting blank view for the admin panel.

---
+++ resources/views/admin/blank.blade.php
@@ -0,0 +1,7 @@
+@extends('admin.master.layout')
+
+@section('title') Blank Page @endsection
+
+@section('content')
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquam, architecto, aspernatur blanditiis delectus dolorem illo incidunt inventore minus nesciunt officiis provident quasi reiciendis rerum suscipit tempore voluptate voluptates voluptatibus?</p>
+@endsection
