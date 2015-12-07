daf768d0182c3765d27c862290292ac00ddaf817
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-07 08:52:44 +0100

    Added the MediaUploader observer.

---
+++ app/Entities/Observers/MediaUploader.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace LaravelItalia\Entities\Observers;
+
+
+use Illuminate\Http\Request;
+use LaravelItalia\Entities\Media;
+
+class MediaUploader
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function creating(Media $media)
+    {
+        $uploadedFile = $this->request->file('media');
+        $fileName = time() . $uploadedFile->getClientOriginalName() . $uploadedFile->getClientOriginalExtension();
+
+        \Storage::put(
+            $fileName,
+            $this->getFileContents($uploadedFile->getRealPath())
+        );
+
+        $media->setUrl($fileName);
+    }
+
+    private function getFileContents($realPath)
+    {
+        return file_get_contents($realPath);
+    }
+}
---
+++ tests/unit/Entities/Observers/MediaUploaderTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Http\Request;
+use LaravelItalia\Entities\Media;
+use LaravelItalia\Entities\Observers\MediaUploader;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class MediaUploaderTest extends TestCase
+{
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject|Media
+     */
+    private $mediaMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject|Request
+     */
+    private $requestMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject|UploadedFile
+     */
+    private $uploadedFileMock;
+
+    public function setUp()
+    {
+        $this->mediaMock = $this->getMock(Media::class);
+        $this->requestMock = $this->getMock(Request::class);
+        $this->uploadedFileMock = $this->getMockBuilder(UploadedFile::class)->disableOriginalConstructor()->getMock();
+
+        parent::setUp();
+    }
+
+    public function testUpload()
+    {
+        $this->uploadedFileMock->expects($this->once())
+            ->method('getClientOriginalName')
+            ->willReturn('original_name');
+
+        $this->uploadedFileMock->expects($this->once())
+            ->method('getClientOriginalExtension')
+            ->willReturn('.jpg');
+
+        $this->uploadedFileMock->expects($this->once())
+            ->method('getRealPath')
+            ->willReturn(tempnam('tmp', 'test'));
+
+        $this->requestMock->expects($this->once())
+            ->method('file')
+            ->willReturn($this->uploadedFileMock);
+
+        $expectedFileName = time() . 'original_name.jpg';
+
+        $this->mediaMock->expects($this->once())
+            ->method('setUrl')
+            ->with($this->equalTo($expectedFileName));
+
+        \Storage::shouldReceive('put')->once();
+
+        $mediaUploader = new MediaUploader($this->requestMock);
+        $mediaUploader->creating($this->mediaMock);
+    }
+}
