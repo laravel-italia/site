1709f5346bc8cedf9810a01f150c0b532e9c7583
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-08 22:10:40 +0100

    Added a test auth route. Going to be deleted in the future.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,11 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::controller('/', 'MainController');
 });
 
+Route::get('auth', function(\LaravelItalia\Entities\Repositories\UserRepository $userRepository){
+    Auth::login($userRepository->findFirstBy(['name' => 'Francesco Malatesta']));
+    return redirect('admin/articles');
+});
+
 Route::get('blank', function () {
     return view('admin.blank');
 });
