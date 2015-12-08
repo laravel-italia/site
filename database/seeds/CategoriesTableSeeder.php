5a6570c02362813c4097440a3502e6772c93c93b
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-08 20:02:17 +0100

    Added basic categories table seeder.

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use LaravelItalia\Entities\Repositories\CategoryRepository;
+use LaravelItalia\Entities\Factories\CategoryFactory;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $repository = new CategoryRepository();
+
+        $repository->save(CategoryFactory::createCategory('Package'));
+        $repository->save(CategoryFactory::createCategory('Tutorial'));
+        $repository->save(CategoryFactory::createCategory('News'));
+        $repository->save(CategoryFactory::createCategory('Interviste'));
+        $repository->save(CategoryFactory::createCategory('Tips'));
+        $repository->save(CategoryFactory::createCategory('Risorse'));
+
+        Model::reguard();
+    }
+}
