94297749a365b803afc7e65badaada8c66a1ca28
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-08 01:02:14 +0100

    Added a separate file for the side menu.

---
+++ resources/views/admin/includes/menu.blade.php
@@ -0,0 +1,75 @@
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="{{ url('admin') }}">Amministrazione :: Laravel-Italia.it</a>
+    </div>
+    <!-- /.navbar-header -->
+
+    <ul class="nav navbar-top-links navbar-right">
+        <!-- /.dropdown -->
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="{{ url('/') }}"><i class="fa fa-rocket fa-fw"></i> Vai al Sito</a>
+                </li>
+                <li class="divider"></li>
+                <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                </li>
+            </ul>
+            <!-- /.dropdown-user -->
+        </li>
+        <!-- /.dropdown -->
+    </ul>
+    <!-- /.navbar-top-links -->
+
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+                <li>
+                    <a href="{{ url('admin') }}"><i class="fa fa-dashboard fa-fw"></i> Pagina Principale</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-pencil fa-fw"></i> Articoli<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#">Scrivi Nuovo</a>
+                        </li>
+                        <li>
+                            <a href="#">Elenco</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-th-list fa-fw"></i> Serie<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="#">Aggiungi Nuova</a>
+                        </li>
+                        <li>
+                            <a href="#">Elenco</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-files-o fa-fw"></i> Categorie</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-upload fa-fw"></i> Media</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-group fa-fw"></i> Utenti</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.sidebar-collapse -->
+    </div>
+    <!-- /.navbar-static-side -->
+</nav>
