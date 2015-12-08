36cf15aa910d4e7c66d2156c09c6f6d569caa59d
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-08 20:03:42 +0100

    Added basic series table seeder.

---
+++ database/seeds/SeriesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use LaravelItalia\Entities\Repositories\SeriesRepository;
+use LaravelItalia\Entities\Factories\SeriesFactory;
+
+class SeriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $repository = new SeriesRepository();
+
+        $repository->save(SeriesFactory::createSeries('Serie 1', 'Lorem ipsum...', 'Meta lorem ipsum...', false, false));
+        $repository->save(SeriesFactory::createSeries('Serie 2', 'Lorem ipsum...', 'Meta lorem ipsum...', false, false));
+        $repository->save(SeriesFactory::createSeries('Serie 3', 'Lorem ipsum...', 'Meta lorem ipsum...', false, false));
+        $repository->save(SeriesFactory::createSeries('Serie 4', 'Lorem ipsum...', 'Meta lorem ipsum...', false, false));
+
+        Model::reguard();
+    }
+}
