d0e3ef3aaaddfa737c7a7ceba5f4c8383381ef56
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-13 09:38:54 +0100

    Added role checking protection to the admin MainController

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -6,6 +6,12 @@ use LaravelItalia\Http\Controllers\Controller;
 
 class MainController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('role:editor,administrator');
+    }
+
     public function getDashboard()
     {
         return view('admin.dashboard');
