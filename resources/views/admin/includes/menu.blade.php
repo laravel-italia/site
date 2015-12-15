3e91fd5f61e5fbdfdbef2b58c6e606ee91370e98
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-15 17:06:21 +0100

    Added the user name to the upper right corner dropdown.

--- resources/views/admin/includes/menu.blade.php
+++ resources/views/admin/includes/menu.blade.php
@@ -15,13 +15,13 @@
         <!-- /.dropdown -->
         <li class="dropdown">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                {{ Auth::user()->name }} <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
             </a>
             <ul class="dropdown-menu dropdown-user">
                 <li><a href="{{ url('/') }}"><i class="fa fa-rocket fa-fw"></i> Vai al Sito</a>
                 </li>
                 <li class="divider"></li>
-                <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                <li><a href="{{ url('logout') }}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                 </li>
             </ul>
             <!-- /.dropdown-user -->
