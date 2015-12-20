cf94bb9c90076ab3a761a0f0bf8ab2c8f7f1dd49
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-20 19:42:28 +0100

    Added SeriesSaveRequest class.

---
+++ app/Http/Requests/SeriesSaveRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LaravelItalia\Http\Requests;
+
+use LaravelItalia\Http\Requests\Request;
+
+class SeriesSaveRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'description' => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'Specifica un titolo per la serie!',
+            'description.required' => 'Devi specificare una descrizione della serie!',
+        ];
+    }
+}
