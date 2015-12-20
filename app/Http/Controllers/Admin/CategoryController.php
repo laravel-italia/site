9b0d193cae189ad1a84508ba348e525ea0209008
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-20 19:37:21 +0100

    Added the auth middleware.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -16,6 +16,7 @@ class CategoryController extends Controller
 {
     public function __construct()
     {
+        $this->middleware('auth');
         $this->middleware('role:administrator');
     }
 
