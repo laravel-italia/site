8eca5fd329b0c1121e84daa67bf4d8f20d1bf8d3
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-20 19:25:48 +0100

    Created the media factory class.

---
+++ app/Entities/Factories/MediaFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace LaravelItalia\Entities\Factories;
+
+use LaravelItalia\Entities\Media;
+
+/**
+ * Class MediaFactory.
+ */
+class MediaFactory
+{
+    /**
+     * Creates a new Media instance.
+     *
+     * @return Media
+     */
+    public static function createMedia()
+    {
+        return new Media();
+    }
+}
