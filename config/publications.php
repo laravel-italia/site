4599504e676e4b6cdf264cd4a1858e5214280a9e
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-20 22:18:27 +0100

    Changed settings file name and structure.

--- config/publications.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'articles_per_page' => env('ARTICLES_PER_PAGE'),
-
-    'media_per_page' => env('MEDIA_PER_PAGE')
-];
---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'publications' => [
+        'articles_per_page' => env('ARTICLES_PER_PAGE'),
+        'media_per_page' => env('MEDIA_PER_PAGE')
+    ],
+
+    'user' => [
+        'users_per_page' => env('USERS_PER_PAGE')
+    ]
+];
