723927cbba2dfa7604ccf5fb19da22b07b7994c3
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-26 15:42:53 +0100

    Added the TagRepository class, with tests.

---
+++ app/Entities/Repositories/TagRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace LaravelItalia\Entities\Repositories;
+
+use Illuminate\Database\Eloquent\Collection;
+use LaravelItalia\Entities\Tag;
+use LaravelItalia\Exceptions\NotFoundException;
+use LaravelItalia\Exceptions\NotSavedException;
+use LaravelItalia\Exceptions\NotDeletedException;
+
+/**
+ * Class TagRepository.
+ */
+class TagRepository
+{
+    public function getAll()
+    {
+        return Tag::all();
+    }
+
+    public function getBySlugs(array $slugs)
+    {
+        if(empty($slugs))
+            return new Collection();
+
+        $tag = Tag::query();
+
+        foreach($slugs as $slug){
+            $tag->orWhere('slug', $slug);
+        }
+
+        return $tag->get();
+    }
+
+    public function findById($id)
+    {
+        $tag = Tag::find($id);
+
+        if(!$tag)
+            throw new NotFoundException;
+
+        return $tag;
+    }
+
+    public function findBySlug($slug)
+    {
+        $tag = Tag::where('slug', $slug)->first();
+
+        if(!$tag)
+            throw new NotFoundException;
+
+        return $tag;
+    }
+
+    public function save(Tag $tag)
+    {
+        if(!$tag->save())
+            throw new NotSavedException;
+    }
+
+    public function delete(Tag $tag)
+    {
+        if(!$tag->delete())
+            throw new NotDeletedException;
+    }
+}
---
+++ tests/integration/Repositories/TagRepositoryTest.php
@@ -0,0 +1,118 @@
+<?php
+
+use LaravelItalia\Entities\Tag;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use LaravelItalia\Entities\Repositories\TagRepository;
+
+class TagRepositoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @var TagRepository
+     */
+    private $repository;
+
+    public function setUp()
+    {
+        $this->repository = new TagRepository();
+        parent::setUp();
+    }
+
+    public function testCanSave()
+    {
+        $this->repository->save($this->prepareTestTag('Test'));
+
+        $this->seeInDatabase('tags', [
+            'name' => 'Test',
+            'slug' => 'test',
+        ]);
+    }
+
+    public function testCanFindById()
+    {
+        $expectedTag = $this->saveTestTag('Test');
+
+        $existingTag = $this->repository->findById($expectedTag->id);
+
+        $this->assertNotNull($existingTag);
+    }
+
+    /**
+     * @expectedException           LaravelItalia\Exceptions\NotFoundException
+     */
+    public function testCanFindByIdThrowsException()
+    {
+        $this->repository->findById(999);
+    }
+
+    public function testCanFindBySlug()
+    {
+        $this->saveTestTag('Test');
+
+        $existingCategory = $this->repository->findBySlug('test');
+
+        $this->assertNotNull($existingCategory);
+    }
+
+    /**
+     * @expectedException           LaravelItalia\Exceptions\NotFoundException
+     */
+    public function testCanFindBySlugThrowsException()
+    {
+        $this->repository->findBySlug('not-existing-tag');
+    }
+
+    public function testCanGetAll()
+    {
+        $this->assertEmpty($this->repository->getAll());
+
+        $this->saveTestTag('Test');
+
+        $this->assertCount(1, $this->repository->getAll());
+    }
+
+    public function testCanGetBySlugs()
+    {
+        $this->saveTestTag('Test1');
+        $this->saveTestTag('Test2');
+
+        $this->assertCount(0, $this->repository->getBySlugs([]));
+        $this->assertCount(0, $this->repository->getBySlugs(['tag1', 'tag2']));
+        $this->assertCount(1, $this->repository->getBySlugs(['test1']));
+        $this->assertCount(2, $this->repository->getBySlugs(['test1', 'test2']));
+    }
+
+    public function testCanDelete()
+    {
+        $tag = $this->saveTestTag('Test');
+
+        $this->seeInDatabase('tags', [
+            'name' => 'Test'
+        ]);
+
+        $this->repository->delete($tag);
+
+        $this->dontSeeInDatabase('tags', [
+            'name' => 'Test'
+        ]);
+    }
+
+    private function prepareTestTag($tagName)
+    {
+        $tag = new Tag;
+
+        $tag->name = $tagName;
+        $tag->slug = \Illuminate\Support\Str::slug($tagName);
+
+        return $tag;
+    }
+
+    private function saveTestTag($tagName)
+    {
+        $testTag = $this->prepareTestTag($tagName);
+        $testTag->save();
+
+        return $testTag;
+    }
+}
