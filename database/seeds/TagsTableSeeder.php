f19368f867d19fb59179e5f81edab1c447ae7516
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 10:22:47 +0100

    Added the TagsTableSeeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use LaravelItalia\Entities\Repositories\TagRepository;
+use LaravelItalia\Entities\Factories\TagFactory;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tagRepository = new TagRepository();
+
+        $tagRepository->save(TagFactory::createTag('Presentazioni'));
+        $tagRepository->save(TagFactory::createTag('Laravel'));
+        $tagRepository->save(TagFactory::createTag('Lavoro'));
+        $tagRepository->save(TagFactory::createTag('Progetti'));
+        $tagRepository->save(TagFactory::createTag('Community'));
+
+        $tagRepository->save(TagFactory::createTag('Installazione'));
+        $tagRepository->save(TagFactory::createTag('Configurazione'));
+
+        $tagRepository->save(TagFactory::createTag('Autenticazione'));
+        $tagRepository->save(TagFactory::createTag('Sicurezza'));
+
+        $tagRepository->save(TagFactory::createTag('Richieste'));
+        $tagRepository->save(TagFactory::createTag('Input'));
+        $tagRepository->save(TagFactory::createTag('Risposte'));
+
+        $tagRepository->save(TagFactory::createTag('Sessioni'));
+        $tagRepository->save(TagFactory::createTag('Cache'));
+
+        $tagRepository->save(TagFactory::createTag('Database'));
+        $tagRepository->save(TagFactory::createTag('Eloquent'));
+
+        $tagRepository->save(TagFactory::createTag('Package'));
+        $tagRepository->save(TagFactory::createTag('IoC'));
+
+        $tagRepository->save(TagFactory::createTag('View'));
+        $tagRepository->save(TagFactory::createTag('Blade'));
+        $tagRepository->save(TagFactory::createTag('Form'));
+
+        $tagRepository->save(TagFactory::createTag('Mail'));
+        $tagRepository->save(TagFactory::createTag('Code'));
+    }
+}
