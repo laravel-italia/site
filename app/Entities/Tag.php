07518cedde9b6de4317f64d1aa6c8c5518a9f7e4
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 16:18:18 +0100

    Updates to entities for relationships

--- app/Entities/Tag.php
+++ app/Entities/Tag.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    public function threads()
+    {
+        return $this->belongsToMany(Thread::class);
+    }
 }
--- app/Entities/Thread.php
+++ app/Entities/Thread.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
