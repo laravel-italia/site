0653dfe3e1dd7b5534ee09d3733e2a4da50f5d5c
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 10:34:08 +0100

    Admin can now add new tags to the forum

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -2,8 +2,11 @@
 
 namespace LaravelItalia\Http\Controllers\Admin;
 
+use LaravelItalia\Entities\Factories\TagFactory;
 use LaravelItalia\Entities\Repositories\TagRepository;
+use LaravelItalia\Exceptions\NotSavedException;
 use LaravelItalia\Http\Controllers\Controller;
+use LaravelItalia\Http\Requests\TagAddRequest;
 
 class TagController extends Controller
 {
@@ -12,5 +15,18 @@ class TagController extends Controller
         $tags = $tagRepository->getAll();
 
         return view('admin.tag_index', compact('tags'));
+    }
+
+    public function postAdd(TagAddRequest $request, TagRepository $tagRepository)
+    {
+        $tag = TagFactory::createTag($request->get('name'));
+
+        try {
+            $tagRepository->save($tag);
+        } catch (NotSavedException $e) {
+            return redirect('admin/tags')->with('error_message', 'Problemi in fase di aggiunta del tag. Riprovare.');
+        }
+
+        return redirect('admin/tags')->with('success_message', 'Tag aggiunto correttamente.');
     }
 }
--- resources/views/admin/tag_index.blade.php
+++ resources/views/admin/tag_index.blade.php
@@ -7,6 +7,8 @@
 
     <hr>
 
+    <p><button class="btn btn-success" id="add_button">+ Aggiungi Tag</button></p>
+
     <div class="table-responsive">
         <table class="table table-striped table-bordered table-hover">
             <thead>
@@ -38,4 +40,43 @@
             </tbody>
         </table>
     </div>
+
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Chiudi"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="publishModalLabel">Aggiungi Tag</h4>
+                </div>
+                <form id="article_publish_form" action="{{ url('admin/tags/add') }}" method="post">
+                    {!! csrf_field() !!}
+                    <div class="modal-body">
+                        <p>Scegli il nome del tag da aggiungere.</p>
+                        <p><input type="text" class="form-control" name="name" id="name" required /></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success"><span class="fa fa-check"></span> Aggiungi</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-remove"></span> Annulla</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function(){
+
+            $('#addModal').on('shown.bs.modal', function(){
+                $('#name').focus();
+            });
+
+            $('#add_button').click(function(){
+                $('#name').val('');
+                $('#addModal').modal('toggle');
+            });
+
+        });
+    </script>
 @endsection
