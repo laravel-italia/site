20114ee8f517928959dbf4a882a22ed5c9a17793
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 10:52:47 +0100

    TagAddRequest is now TagSaveRequest, to be used also with edit

--- app/Http/Requests/TagSaveRequest.php
+++ app/Http/Requests/TagSaveRequest.php
@@ -2,7 +2,7 @@
 
 namespace LaravelItalia\Http\Requests;
 
-class TagAddRequest extends Request
+class TagSaveRequest extends Request
 {
     /**
      * Determine if the user is authorized to make this request.
