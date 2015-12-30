973fdb760d8544d05e2d629b1e824348a430211d
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 10:33:39 +0100

    Added the TagAddRequest

---
+++ app/Http/Requests/TagAddRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaravelItalia\Http\Requests;
+
+class TagAddRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Specifica il nome del tag!',
+        ];
+    }
+}
