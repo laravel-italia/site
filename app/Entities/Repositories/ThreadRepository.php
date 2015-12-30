889209945be5a2275d836ac7196687a9ad7ab904
Francesco Malatesta <hellofrancesco@gmail.com>
2015-12-30 16:19:24 +0100

    Added the ThreadRepository with tests

---
+++ app/Entities/Repositories/ThreadRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LaravelItalia\Entities\Repositories;
+
+use LaravelItalia\Entities\Thread;
+use Illuminate\Database\Eloquent\Collection;
+use LaravelItalia\Exceptions\NotSavedException;
+use LaravelItalia\Exceptions\NotDeletedException;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+/**
+ * Class ThreadRepository.
+ */
+class ThreadRepository
+{
+    use DatabaseMigrations;
+
+    public function getAll($page)
+    {
+        $query = Thread::with(['tags'])->orderBy('created_at', 'DESC');
+
+        return $query->paginate(
+            \Config::get('settings.forum.threads_per_page'),
+            ['*'],
+            'page',
+            $page
+        );
+    }
+
+    public function getByTags(Collection $tags, $page)
+    {
+        $query = Thread::with(['tags'])
+            ->getQuery()
+            ->join('tag_thread', 'threads.id', '=', 'tag_thread.thread_id')
+            ->whereIn(
+                'tag_thread.tag_id',
+                $tags->lists('id')
+            );
+
+        $query->groupBy('threads.id')
+            ->orderBy('threads.created_at', 'DESC');
+
+        return $query->paginate(
+            \Config::get('settings.forum.threads_per_page'),
+            ['*'],
+            'page',
+            $page
+        );
+    }
+
+    public function save(Thread $thread)
+    {
+        if (!$thread->save()) {
+            throw new NotSavedException();
+        }
+    }
+
+    public function delete(Thread $thread)
+    {
+        if (!$thread->delete()) {
+            throw new NotDeletedException();
+        }
+    }
+}
