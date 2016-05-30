241ae9aa6d74482beaec68b715536c1d36794833
Francesco Malatesta <hellofrancesco@gmail.com>
2016-05-30 16:30:09 +0200

    Seed only admin and editor roles

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -16,9 +16,5 @@ class RolesTableSeeder extends Seeder
         $editorRole = new \LaravelItalia\Domain\Role();
         $editorRole->name = 'editor';
         $editorRole->save();
-
-        $userRole = new \LaravelItalia\Domain\Role();
-        $userRole->name = 'user';
-        $userRole->save();
     }
 }
