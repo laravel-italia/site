cca349ac92234df261e0053d397a864430f34658
Francesco Malatesta <hellofrancesco@gmail.com>
2016-06-17 21:07:26 +0200

    Edit mantainance view

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,47 +1,19 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Be right back.</title>
+@extends('front.layout.master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@section('head')
+    <title>In Manutenzione :: Laravel-Italia.it</title>
+@endsection
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Be right back.</div>
+@section('contents')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <article class="not-found">
+                    <h2 class="w_light pt40">Ehilà! Puoi ripassare dopo?</h2>
+                    <h2 class="pt60 pb50"><b>;)</b></h2>
+                    <h6 class="subtitle">Ci sono dei lavori di manutenzione in corso, da queste parti... grazie per la pazienza!</h6>
+                </article>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
