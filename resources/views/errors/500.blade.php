f17c546b6e62b0a158399cde58a788f30c8ada85
Francesco Malatesta <hellofrancesco@gmail.com>
2016-06-29 22:01:14 +0200

    Add a generic 500 error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,19 @@
+@extends('front.layout.master')
+
+@section('head')
+    <title>Errore! :: Laravel-Italia.it</title>
+@endsection
+
+@section('contents')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <article class="not-found">
+                    <h2 class="w_light pt40">Mmmh... qualcosa non va.</h2>
+                    <h2 class="pt60 pb50"><b>:\</b></h2>
+                    <h6 class="subtitle">A quanto pare qualcosa è andato storto durante la tua richiesta. Prova di nuovo tra un po'!</h6>
+                </article>
+            </div>
+        </div>
+    </div>
+@endsection
