4323ecf08f20c5416010606c35cc04b4a086505b
Francesco Malatesta <hellofrancesco@gmail.com>
2017-01-15 20:01:17 +0100

    Fix mail sender address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'info@laravel-italia.com',
+        'name' => 'Laravel-Italia',
     ],
 
     /*
